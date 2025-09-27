f72879b37c40c163106fba66ef9be8e32fb38d95
srijan2025-hub <srijangithub@gmail.com>
2025-09-27 17:18:32 +0530

    Update arihantdcpandey.js
    Signed-off-by: srijan2025-hub <srijangithub@gmail.com>

--- phy/arihantdcpandey.js
+++ phy/arihantdcpandey.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       title: "DC Pandey Mechanics 1",
       pdf: "https://raw.githubusercontent.com/srijan2025-hub/physicsstorage1/main/DCPandeyMechanics1.pdf",
-      gdrive: "1Ft7LyjA3FLoRYhTCDHWPEu_aYbhWaeau", // optional
+      gdrive: "", // optional
       id: "preview1"
     },
     {
